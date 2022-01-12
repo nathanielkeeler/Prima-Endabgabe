bde5e74f6a208ef1a27c0f5f60f37ce33a2643f8
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2022-01-12 12:27:13 +0100

    removed physics. causing issues with 2D elements

--- Script/Source/Main.ts
+++ Script/Source/Main.ts
@@ -5,9 +5,9 @@ namespace FlappyBug {
   let root: ƒ.Graph;
   let viewport: ƒ.Viewport;
   let player: ƒ.Node;
-  let playerBody: ƒ.ComponentRigidbody;
+  // let playerBody: ƒ.ComponentRigidbody;
 
-  let ctrFlap: ƒ.Control = new ƒ.Control("Flap", 3, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  let ctrFlap: ƒ.Control = new ƒ.Control("Flap", 1, ƒ.CONTROL_TYPE.PROPORTIONAL);
 
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
@@ -18,7 +18,7 @@ namespace FlappyBug {
     root = <ƒ.Graph>ƒ.Project.resources["Graph|2021-12-20T18:00:23.325Z|85852"];
 
     player = root.getChildrenByName("Player")[0];
-    playerBody = player.getComponent(ƒ.ComponentRigidbody);
+    // playerBody = player.getComponent(ƒ.ComponentRigidbody);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -27,12 +27,12 @@ namespace FlappyBug {
 
   function update(_event: Event): void {
 
-    let flap: number = ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.S]);
-    ctrFlap.setInput(flap);
-    playerBody.applyForce(ƒ.Vector3.SCALE(player.mtxLocal.getY(), ctrFlap.getOutput()));
+    // let flap: number = ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.S]);
+    // ctrFlap.setInput(flap);
+    // playerBody.applyForce(ƒ.Vector3.SCALE(player.mtxLocal.getY(), ctrFlap.getOutput()));
 
 
-    ƒ.Physics.world.simulate();  // if physics is included and used
+    // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
